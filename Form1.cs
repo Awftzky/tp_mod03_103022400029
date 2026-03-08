f824bb17fc438d2a8b4914674bc330176d5b6161
Awftzky <Aufaazzakyarief@gmail.com>
2026-03-09 06:04:55 +0800

    last commit

--- Form1.cs
+++ Form1.cs
@@ -2,8 +2,29 @@ namespace TP_Modul3_103022400029;
 
 public partial class Form1 : Form
 {
+    private int result = 0;
+
     public Form1()
     {
         InitializeComponent();
+    }
+
+    private void btnPlus_Click(object sender, EventArgs e)
+    {
+        if (!string.IsNullOrEmpty(lblOutput.Text))
+        {
+            result += int.Parse(lblOutput.Text);
+            lblOutput.Text = ""; // Kosongkan layar untuk angka berikutnya
+        }
+    }
+
+    private void btnEqual_Click(object sender, EventArgs e)
+    {
+        if (!string.IsNullOrEmpty(lblOutput.Text))
+        {
+            result += int.Parse(lblOutput.Text);
+            lblOutput.Text = result.ToString();
+            result = 0; // Reset setelah selesai
+        }
     }
 }
